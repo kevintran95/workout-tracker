a36e8c7552aff9da0eb11d38edbce3dfd5f66c0b
Kevin Tran <kevin_tran95@yahoo.com>
2021-10-11 19:52:19 -0700

    completed workout model

--- models/Workout.js
+++ models/Workout.js
@@ -1,1 +1,47 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  exercises: [
+    {
+      type: {
+        type: String
+      },
+      name: {
+        type: String
+      },
+      duration: {
+        type: Number
+      },
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      },
+    }
+  ],
+  day: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = WorkoutSchema
+
+
+
+
+
+
+
+
+
+
+
